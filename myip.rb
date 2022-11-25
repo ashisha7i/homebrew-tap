ceefa33c45389efff77ea0de9c322149317466b9
Ashish <agnihotri.ashish@gmail.com>
2022-11-25 11:13:24 -0800

    Updated with current SHA256

--- myip.rb
+++ myip.rb
@@ -2,7 +2,7 @@ class Myip < Formula
   desc "Simple Homebrew formula for finding public ip address (shell script)"
   homepage ""
   url "https://github.com/ashisha7i/myip-hbf/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "2286507e65aeef6612d82ce93b14e6f2f83d5501837ce9600c4da0368a19deae"
+  sha256 "5f5b3bae86639d346998f6aa981fec8ab58ffb1b3b7a1cb8a044896c14062499"
   license "MIT"
 
 
