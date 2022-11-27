6ff3c2e2991dc8119357c3a690cbcf68a602e4ab
Ashish <agnihotri.ashish@gmail.com>
2022-11-27 02:12:38 -0800

    Update myip.rb

--- myip.rb
+++ myip.rb
@@ -1,13 +1,14 @@
 class Myip < Formula
   desc "Simple Homebrew formula for finding public ip address (shell script)"
   homepage "https://github.com/ashisha7i/homebrew-formulae"
-  url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "dee11b2a847315c6a20939eff9c7fea0ce21600fafa986e76c52fb4d16381dd9"
+  url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v.0.0.2.tar.gz"
+  sha256 "f9d6f44efa127c75ef67d4140afca404d6399b973f91db6da5a9d4beff5c38d5"
   license "MIT"
 
 
   def install
     bin.install "bin/myip"
+    bin.install "bin/gpvpn"
   end
 
 end
