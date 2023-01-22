4cd924bd52e1279c1292f32bde71cc5a591511de
Ashish <agnihotri.ashish@gmail.com>
2023-01-21 23:54:41 -0800

    Locutil updated

--- lockutil.rb
+++ lockutil.rb
@@ -1,8 +1,8 @@
 class Lockutil < Formula
     desc "Simple Homebrew Formula to lock/unlock a file (only on Mac - uses chflags command internally)"
     homepage "https://github.com/ashisha7i/homebrew-formulae"
-    url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v0.0.3.tar.gz"
-    sha256 "4160ba09f724941fdb8d6f0d6310eefea83e69afd5e8dbab4211f73dd400734f"
+    url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v0.0.4.tar.gz"
+    sha256 "3ed509024ef7800f954053e65388090fb6bd0db04a1751d898b5ea9e94942ecf"
     license "MIT"
   
   
