40f567bbf090b71964981eb6f4a39dfe7aa114ef
Ashish <agnihotri.ashish@gmail.com>
2023-01-21 23:49:30 -0800

    Updated SHA

--- lockutil.rb
+++ lockutil.rb
@@ -2,7 +2,7 @@ class Lockutil < Formula
     desc "Simple Homebrew Formula to lock/unlock a file (only on Mac - uses chflags command internally)"
     homepage "https://github.com/ashisha7i/homebrew-formulae"
     url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v0.0.3.tar.gz"
-    sha256 "f9d6f44efa127c75ef67d4140afca404d6399b973f91db6da5a9d4beff5c38d5"
+    sha256 "4160ba09f724941fdb8d6f0d6310eefea83e69afd5e8dbab4211f73dd400734f"
     license "MIT"
   
   
