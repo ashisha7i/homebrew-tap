5e76d8400f103f04bcbf6f18214e11d11240111e
Ashish <agnihotri.ashish@gmail.com>
2023-01-21 23:39:30 -0800

    Adding lockutil

---
+++ lockutil.rb
@@ -0,0 +1,14 @@
+class Lockutil < Formula
+    desc "Simple Homebrew Formula to lock/unlock a file (only on Mac - uses chflags command internally)"
+    homepage "https://github.com/ashisha7i/homebrew-formulae"
+    url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v.0.0.3.tar.gz"
+    sha256 "f9d6f44efa127c75ef67d4140afca404d6399b973f91db6da5a9d4beff5c38d5"
+    license "MIT"
+  
+  
+    def install
+      bin.install "bin/lockutil"
+    end
+  
+  end
+  
