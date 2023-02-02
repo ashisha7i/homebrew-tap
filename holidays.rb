b8367187473e5e63c76c70aae24d38509a01fd34
Ashish Agnihotri <aagnihotri@mgmresorts.com>
2023-02-02 13:17:31 -0800

    Updated holidays formula

--- holidays.rb
+++ holidays.rb
@@ -1,8 +1,8 @@
 class Holidays < Formula
     desc "Simple Homebrew formula to show the holidays for the year 2023"
     homepage "https://github.com/ashisha7i/homebrew-formulae"
-    url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v1.0.4.tar.gz"
-    sha256 "e20a4ad777fd68767e82199599c5bdba85ecaced1f66f18dc2e43d30e1e0821e"
+    url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v1.0.5.tar.gz"
+    sha256 "52543af03c5f6409eb173f2cffe29d8ba7efcb895b074658b92fede093298874"
     license "MIT"
   
   
