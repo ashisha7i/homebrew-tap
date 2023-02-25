bec181e7c3b3ba285d489bd607e0b32061c3203e
Ashish Agnihotri <aagnihotri@mgmresorts.com>
2023-02-25 02:22:41 -0800

    New release for 'mgmprops'

--- mgmprops.rb
+++ mgmprops.rb
@@ -3,8 +3,8 @@ class Mgmprops < Formula
     depends_on "jq" 
     desc "Installs 'mgmprop' command that can be used to fetch details for MGM properties"
     homepage "https://github.com/ashisha7i/homebrew-formulae"
-    url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v1.0.3.tar.gz"
-    sha256 "8c54e019e8901a4d3878ba670290755967defdca7613daebfe25c901d5f51d50"
+    url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v1.0.6.tar.gz"
+    sha256 "18a26a56331b23e9eb9ea99d6bb47bfc592fa85b0982216224ee1c739139213b"
     license "MIT"
   
   
