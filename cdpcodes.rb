76adf715e231b0b0c220404d60661ec38640aea5
Ashish <agnihotri.ashish@gmail.com>
2023-11-03 19:54:14 -0700

    Update cdpcodes.rb with new release for ENT code

--- cdpcodes.rb
+++ cdpcodes.rb
@@ -2,7 +2,7 @@ class Cdpcodes < Formula
     depends_on "jq" 
     desc "Installs 'cdpcodes' command that can be used to fetch details for CSP codes for MGM properties"
     homepage "https://github.com/ashisha7i/homebrew-formulae"
-    url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v1.0.7.tar.gz"
+    url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v1.0.8.tar.gz"
     sha256 "6c1316bfbbd85a143938387eed96584259d59e3da885ebfa9ba1c92e01f390a2"
     license "MIT"
   
