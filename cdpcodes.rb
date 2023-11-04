4d4ae1acf7b7e53868bc103a83fb1eb21e03e0ae
Ashish <agnihotri.ashish@gmail.com>
2023-11-03 20:18:46 -0700

    Update cdpcodes.rb

--- cdpcodes.rb
+++ cdpcodes.rb
@@ -3,7 +3,7 @@ class Cdpcodes < Formula
     desc "Installs 'cdpcodes' command that can be used to fetch details for CSP codes for MGM properties"
     homepage "https://github.com/ashisha7i/homebrew-formulae"
     url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v1.0.8.tar.gz"
-    sha256 "6c1316bfbbd85a143938387eed96584259d59e3da885ebfa9ba1c92e01f390a2"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     license "MIT"
   
   
