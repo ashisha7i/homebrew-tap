e6579e62128e278418427d01980b647e93d603b6
Ashish <agnihotri.ashish@gmail.com>
2023-11-09 17:47:50 -0800

    Update holidays.rb for 2024

--- holidays.rb
+++ holidays.rb
@@ -1,10 +1,10 @@
 class Holidays < Formula
     depends_on "dateutils"
 
-    desc "Simple Homebrew formula to show the holidays for the year 2023"
+    desc "Simple Homebrew formula to show the holidays for the year 2023 and 2024"
     homepage "https://github.com/ashisha7i/homebrew-formulae"
-    url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v1.0.5.tar.gz"
-    sha256 "52543af03c5f6409eb173f2cffe29d8ba7efcb895b074658b92fede093298874"
+    url "https://github.com/ashisha7i/homebrew-formulae/archive/refs/tags/v1.0.10.tar.gz"
+    sha256 "2adc617feb2d648c09cf4fbd58884b22f59efb52ee4416545390e202d87c8ad8"
     license "MIT"
   
   
